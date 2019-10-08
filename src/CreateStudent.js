6af96904dc92528f61c7c99501643aa15562638f
Dominique <dominiqueboyer94@yahoo.com>
2019-10-08 12:00:59 -0700

    create works so im going to leave it alone haha

--- src/CreateStudent.js
+++ src/CreateStudent.js
@@ -10,7 +10,6 @@ class _CreateStudent extends React.Component{
       lastName: '',
       email: '',
       GPA:'',
-      schoolId: '',
       error:''
     };
     this.create = this.create.bind(this);
@@ -21,11 +20,11 @@ class _CreateStudent extends React.Component{
     this.setState({firstName: '',
     lastName: '',
     email: '',
-    GPA:'',
-    schoolId:''})
+    GPA:''
+    })
   }
   render(){
-    const { firstName, lastName, email, GPA, schoolId, error } = this.state;
+    const { firstName, lastName, email, GPA, error } = this.state;
     return (
 
       <form onSubmit={ev => ev.preventDefault()}>
@@ -33,7 +32,7 @@ class _CreateStudent extends React.Component{
         <label>Last Name <input value={lastName} onChange={(ev) => this.setState({ lastName: ev.target.value})}/></label>
         <label>Email <input type="email" value={email} onChange={(ev) => this.setState({ email: ev.target.value})}/></label>
         <label>GPA <input value={GPA} onChange={(ev) => this.setState({ GPA: ev.target.value})}/></label>
-        <label>Enroll at
+        {/* <label>Enroll at
           <select name="school" value={schoolId} onChange={ev => this.setState({ schoolId: ev.target.value})}>
             <option value="null">Not Enrolled</option>
             <option value="mit">MIT</option>
@@ -42,8 +41,8 @@ class _CreateStudent extends React.Component{
             <option value="ccny">CCNY</option>
             <option value="cp">Cal Poly SLO</option>
           </select>
-        </label>
-        <button onClick={ this.create }>Save</button>
+        </label> */}
+        <button onClick={this.create }>Save</button>
       </form>
     );
   }
