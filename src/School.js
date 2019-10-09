6d52a026213763804fb923c5287f4221b13870d8
Dominique <dominiqueboyer94@yahoo.com>
2019-10-08 23:00:29 -0700

    got students on the schools pages

--- src/School.js
+++ src/School.js
@@ -1,33 +1,61 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { getSchoolsThunk, getStudentsThunk, updateStudentThunk } from './store';
+import { getSchoolsThunk, getStudentsThunk, updateStudentThunk, deleteStudentThunk } from './store';
 
 
 class _School extends React.Component{
   constructor(props){
     super();
+    this.deleteStudent = this.deleteStudent.bind(this);
     // this.updateStudent = this.updateStudent.bind(this);
   }
   async componentDidMount(){
     // const id = this.props.match.params.id
     await this.props.getSchools()
+    await this.props.getStudents()
+  }
+  async deleteStudent(id){
+    await this.props.deleteStudent(id)
   }
   // async updateStudent(){
   //   await this.props.updateStudent()
   // }
   render(){
     const id = this.props.match.params.id
-    const {schools} = this.props;
-    const school = schools.filter( school => school.id === id)
-    console.log(schools)
+    const {schools, students} = this.props;
+    const _schools = schools.map(school => {
+      const studentsAttending = students.filter(student => student.schoolId === school.id)
+      return {...school, studentsAttending}
+    });
     return (
       <div>
         {
-          schools.map(school => school.id=== id ? (<div key={school.id}>
-              <h1>{school.name} (Students Enrolled)</h1>
-              <button>Add Student</button>
-              <div>
-                Students:
+          _schools.map(school => school.id=== id ? (<div key={school.id}>
+              <h2>{school.name} ( { school.studentsAttending.length } Students Enrolled)</h2>
+              <select >
+                <option>Add Student</option>
+                {
+                  students.map(student => student.schoolId !== school.id ? <option key={ student.id} value={student.id}>{student.firstName} {student.lastName} </option> : '')
+                }
+              </select >
+              <div className='students'>
+                {
+                  students.map(student => student.schoolId === school.id ? (<li key={student.id}>
+                    <div> {student.firstName} {student.lastName} </div>
+                    <div> GPA {student.GPA} </div>
+                    <div> Enrolled at {student.schoolId === null ? 'nowhere' : 'somewhere' }</div>
+                    <select onChange={(ev)=> this.updateStudent({id: student.id, schoolId: ev.target.value})}>
+                      <option selected={student.schoolId===null}>Not Enrolled</option>
+                      {
+                        schools.map(school => (
+                          <option key={school.id} value={school.id} selected={student.schoolId===school.id}>{school.name}</option>
+                        ))
+                      }
+                    </select>
+                    <br/>
+                    <button onClick={ () => this.deleteStudent(student.id) }>Delete Student</button>
+                  </li>) : '')
+                }
               </div>
             </div>) : '')
         }
@@ -63,6 +91,7 @@ const mapStateToProps = (state)=> {
 const dispatchToProps = {
   getSchools: getSchoolsThunk,
   getStudents: getStudentsThunk,
+  deleteStudent: deleteStudentThunk
 
 };
 
